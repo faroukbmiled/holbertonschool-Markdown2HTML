a40cc7d51f1086724f4f7636445eb1c68345eb9d
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-23 11:28:40 +0100

    style

--- markdown2html.py
+++ markdown2html.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
+"""markdown2html.py ,markdown converter"""
+
 import sys
 from markdown import markdown
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
 
-file = sys.argv[1]
-out = sys.argv[2]
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
 
-if not os.path.exists(file):
-    print(f"Missing {file}", file=sys.stderr)
-    sys.exit(1)
+    file = sys.argv[1]
+    out = sys.argv[2]
 
-with open(file) as f:
+    if not os.path.exists(file):
+        print(f"Missing {file}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(file) as f:
         markdown_text = f.read()
-html_text = markdown(markdown_text)
+    html_text = markdown(markdown_text)
 
-with open(out, "w") as f:
-    f.write(html_text)
+    with open(out, "w") as f:
+        f.write(html_text)
 
-sys.exit(0)
+    sys.exit(0)
