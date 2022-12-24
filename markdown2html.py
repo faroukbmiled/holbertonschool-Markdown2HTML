031f0743934abb28c987e4db9ec2c9fe69d6bc16
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-25 00:50:14 +0100

    up

--- markdown2html.py
+++ markdown2html.py
@@ -3,6 +3,7 @@
 
 
 import sys
+import os
 
 
 def md_to_html(file, out):
@@ -36,14 +37,18 @@ def md_to_html(file, out):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 3:
-        print(
-            "Usage: ./markdown2html.py README.md README.html", file=sys.stderr
-            )
-        sys.exit(1)
-
     file = sys.argv[1]
     out = sys.argv[2]
 
-    # Convert the markdown to HTML
+    if len(sys.argv) < 3:
+        sys.stderr.write(
+            "Usage: ./markdown2html.py README.md README.html"
+            )
+        sys.exit(1)
+
+    if not os.path.exists(file):
+        print(f"Missing {file}", file=sys.stderr)
+        sys.stderr.write(f"Missing {file}")
+        sys.exit(1)
+
     md_to_html(file, out)
