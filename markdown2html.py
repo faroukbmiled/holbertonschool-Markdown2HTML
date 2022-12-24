63311c817dc97d390029f2802b8cd2b1b642f402
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-25 00:46:42 +0100

    rewriting attempt

--- markdown2html.py
+++ markdown2html.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python3
 """markdown2html.py ,markdown converter"""
 
+
 import sys
-from markdown import markdown
-import os
+
+
+def md_to_html(file, out):
+
+    with open(file, "r", encoding="utf-8") as f:
+        markdown_string = f.read()
+
+    lines = markdown_string.split("\n")
+
+    converted = []
+
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("#"):
+            heading_level = line.count("#")
+            heading_text = line.strip("#").strip()
+
+            converted.append(
+                f"<h{heading_level}>{heading_text}</h{heading_level}>"
+                )
+
+        elif line:
+            converted.append(f"<p>{line}</p>")
+
+    html = "\n".join(converted)
+
+    with open(out, "w", encoding="utf-8") as f:
+        f.write(html)
 
 
 if __name__ == "__main__":
+
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
+        print(
+            "Usage: ./markdown2html.py README.md README.html", file=sys.stderr
+            )
+        sys.exit(1)
 
     file = sys.argv[1]
     out = sys.argv[2]
 
-    if not os.path.exists(file):
-        sys.stderr.write(f"Missing {file}" + "\n")
-        exit(1)
-
-    with open(file) as f:
-        markdown_text = f.read()
-    html_text = markdown(markdown_text)
-
-    with open(out, "w") as f:
-        f.write(html_text)
-
-    exit(0)
+    # Convert the markdown to HTML
+    md_to_html(file, out)
