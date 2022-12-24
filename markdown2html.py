be74ed202573a043cd6af45cfaa7de1cd137d7ca
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-25 00:55:01 +0100

    up

--- markdown2html.py
+++ markdown2html.py
@@ -37,8 +37,6 @@ def md_to_html(file, out):
 
 if __name__ == "__main__":
 
-
-
     if len(sys.argv) < 3:
         sys.stderr.write(
             "Usage: ./markdown2html.py README.md README.html\n"
